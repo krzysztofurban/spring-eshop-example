6d3e7483fd84bbe7023c6d1037051bea0c8c2672
Krzysztof Urban <k.urban@g.pl>
2020-03-09 06:02:15 +0100

    Initial commit

---
+++ src/main/java/pl/krzysztofurban/springeshopexample/SpringEshopExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.krzysztofurban.springeshopexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEshopExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringEshopExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/krzysztofurban/springeshopexample/SpringEshopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzysztofurban.springeshopexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
